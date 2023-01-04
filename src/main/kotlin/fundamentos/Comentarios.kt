f1d2f814cdb44702cf6cd911c408877cea5401b9
WesleyAugusto <ranzerap@gmail.com>
2023-01-04 19:20:23 -0300

    curso kotlin

---
+++ src/main/kotlin/fundamentos/Comentarios.kt
@@ -0,0 +1,9 @@
+package fundamentos
+
+/**
+ * comentario de  varias linhas
+ */
+//comentario de  uma linha
+fun main(args: Array<String>) {
+
+}
---
+++ src/main/kotlin/fundamentos/TemplateString1.kt
@@ -0,0 +1,7 @@
+package fundamentos
+
+
+fun main(args: Array<String>) {
+    val aprovados = listOf("jao","maria","pedro")
+    println("O primeiro colocado foi ${aprovados[1]}:)")
+}
