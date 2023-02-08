d814d5b2d6487b8304b90ec6f1a0d1f577feb175
WesleyAugusto <ranzerap@gmail.com>
2023-02-07 21:08:53 -0300

    fundamentos kotlin

---
+++ src/main/kotlin/classes/TiposVariasveis.kt
@@ -0,0 +1,33 @@
+package classes
+
+val diretmenteNoArquivo = "Bom Dia "
+
+fun toplevel() {
+    val local = "fulano!"
+    println("$diretmenteNoArquivo $local")
+}
+
+class Coisa {
+    var variavelDeInstancia: String = "Boa Noite"
+
+    companion object {
+        @JvmStatic
+        val constanteDeClasse = "ciclano"
+    }
+
+    fun fazer() {
+        val local: Int = 7
+
+        if (local > 3) {
+            val variavelDeBloco = "beltrano"
+            println("$variavelDeInstancia, $constanteDeClasse, $local e $variavelDeBloco!")
+        }
+
+        }
+    }
+
+fun main(args: Array<String>) {
+    toplevel()
+    Coisa().fazer()
+    println(Coisa.constanteDeClasse)
+}
